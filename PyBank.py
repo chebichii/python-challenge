1342695ea9a9a0c9b734bd4bea5e4c98ed6848c0
chebichii <59635047+chebichii@users.noreply.github.com>
2020-02-16 23:04:37 -0600

    Add files via upload

--- PyBank.py
+++ PyBank.py
@@ -1,51 +1,90 @@
 import os
 import csv
 
-csvpath = os.path.join('..', 'Resources','budget_data.csv')
+csvpath = os.path.join('C:/Users/Cece Malachi/Desktop/PyBank_Resources_budget_data.csv')
+outpath = os.path.join('C:/Users/Cece Malachi/Desktop/result/result.txt')
 print(csvpath)
 
 #open and read
 with open(csvpath, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+    csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
-    csv_header = next(csvfile)
+    
+    #specify delimiter
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
     
  
     # Declare variables 
     Total_months = []
     Profit_Loss = []
-    Avg_changes = []
+    Diff = []
     Greatest_Increase_Date = ""
     Greatest_Decrease_Date = ""
+    
     
     # total number of months 
     for row in csvreader:
         Total_months.append(row[0])   
-        Profit_Loss.append(int(row[1]))
+        Profit_Loss.append(row[1])
+        Profit_Loss = [int(i) for i in Profit_Loss]
+        total_change =0
 
-    for i in range(1, len(Profit_Loss)):
+    #print(Profit_Loss)
+        #print (type(Profit_Loss))
+    for i in range(len(Profit_Loss)-1):
+
         
         # Find average change between months
-        Avg_changes.append(Profit_Loss[i] - Profit_Loss[i-1])
+        diff=Profit_Loss[i+1] - Profit_Loss[i]
+        total_change = total_change + diff
         
         # Find the average change
-        Average_Changes = sum(Avg_changes) / len(Avg_changes)
+        Avg_Change = (total_change) / (len(Profit_Loss)-1)
+        #Diff
+    print(Diff)
         
         # find the greatest increase and date
-        Greatest_Increase = max(Avg_changes)
-        Greatest_Increase_Date = str(Months[Avg_changes.index(max(Avg_changes))])
+    Greatest_Increase = max(Profit_Loss)
+    Greatest_Increase_Date = str( Total_months[Profit_Loss.index(max(Profit_Loss))])
         
         
         # find the greatest decrease and date
-        Greatest_Decrease = min(Avg_changes)
-        Greatest_Decrease_Date = str(Months[Avg_changes.index(min(Avg_changes))])    
+    Greatest_Decrease = min(Profit_Loss)
+    Greatest_Decrease_Date = str( Total_months[Profit_Loss.index(min(Profit_Loss))])  
+     #file to write
+      
+
+with open(outpath, 'w') as writer:
+          #delimiter
+    writer.writelines("Financial Analysis")
+    writer.writelines("---------------------------------------------\n")
+
+    writer.writelines("Total Months: " + str(Total_months)+ "\n")
+
+    writer.writelines("Total : $" + str(Profit_Loss) + "\n")
+
+    writer.writelines("Average Change:" + "$" + str(Diff)+ "\n")
+
+    writer.writelines("Greatest Increase:" + Greatest_Decrease_Date + " ($" + str(Greatest_Increase) + ")\n")
+
+    writer.writelines("Greatest Decrease: " + Greatest_Decrease_Date + " ($" + str(Greatest_Decrease) + ")\n")
+          
+
+
+          
+
+
+          
+         
+          
 
 
 # Outputs
 print("Financial Analysis")
 print("------------------")
-print("Total Months: " + str(Total_months))
-print("Total : " +"$" +str(Profit_Loss))      
-print("Average Change: " +"$"+ str(Avg_changes))
-print("Greatest Increase :" + str(max[0])+" ($" + str(max[1])+")")
-print("Greatest Decrease :" + str(min[0])+" ($" + str(min[1])+")")
+print("Total Months:", len(Total_months))
+print("Total : $", sum(Profit_Loss))      
+print("Average Change: $",round(Avg_Change))
+print("Greatest Increase :" + Greatest_Increase_Date + " $" + str(Greatest_Increase))
+print("Greatest Decrease :" + Greatest_Decrease_Date +" $" + str(Greatest_Decrease))
